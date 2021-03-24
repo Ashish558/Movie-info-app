a1367e5b50f1666f80cba2f9884af6f9fcefba04
Ashish558 <77892788+Ashish558@users.noreply.github.com>
2021-03-24 12:12:29 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import SearchBar from './components/SearchBar'
 import Status from './components/Status'
 
 function Home(){
- console.log("home")
+ 
  return(
  <div className="home-page">
     <SearchBar />
