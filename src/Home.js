5e5eeafe920228eaff7f1166deda91685649ec76
Ashish558 <77892788+Ashish558@users.noreply.github.com>
2021-10-23 12:41:49 +0530

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import SearchBar from './components/SearchBar'
+import Status from './components/Status'
+
+function Home(){
+ 
+ return(
+ <div className="home-page">
+    <SearchBar />
+    <Status />
+</div>
+ )
+  
+}
+
+export default Home
